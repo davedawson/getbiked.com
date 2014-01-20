5134aa7519dc036041613d54ca5b863470e6b0d2
davedawson <dave@iamdavedawson.com>
2014-01-19 23:48:53 -0500

    finish style tweaks

--- js/getbiked.js
+++ js/getbiked.js
@@ -16,3 +16,34 @@ $(window).load(function() {
         controlNav: false
       });
 });
+
+jQuery(document).ready(function() {
+    $(".menu").click(function() {
+      $( ".nav-wrap" ).slideToggle( "fast", function() {
+        // Animation complete.
+        $('.menu').toggleClass('open');
+      });
+    });
+  enquire.register("screen and (min-width:972px)", function() {
+
+    //execute some code for large-screen devices
+    // gb.activeNav();
+    $('.nav-wrap').show();
+    $('.nav-wrap').removeClass('small');
+
+
+    console.log('wide');
+
+  }, true);
+  enquire.register("screen and (max-width:971px)", function() {
+
+    //execute some code for small-screen devices
+    $('.nav-wrap').addClass('small');
+    $('.nav-wrap').hide();
+    $('.menu').removeClass('close');
+
+    console.log('small');
+
+  }, true);
+
+});
